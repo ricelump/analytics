9f575809a5d780d114f7b52036508d881e5f712e
Lee <rin.git@preblocks.com>
2026-04-12 19:10:12 +0800

    chore: add ui, lint and fmt

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+	<div>
+		<NuxtRouteAnnouncer />
+		<Button>Test</Button>
+	</div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,18 @@
+import tailwindcss from '@tailwindcss/vite'
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+	modules: ['@nuxtjs/color-mode', '@vueuse/nuxt', 'shadcn-nuxt'],
+	colorMode: {
+		classSuffix: '',
+	},
+	css: ['~/assets/css/main.css'],
+	vite: {
+		plugins: [tailwindcss()],
+	},
+	shadcn: {
+		prefix: '',
+	},
+	compatibilityDate: '2025-07-15',
+	devtools: { enabled: true },
 })
